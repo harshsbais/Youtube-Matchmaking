3ceafec43a061136c7bad3896eec0dd1a76cfa19
Harsh Singh Bais <harshsbais5@gmail.com>
2021-04-03 20:22:11 +0530

    make basic layout based on wireframe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Select from 'react-select';
+import './App.css'
 function App() {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
   return (
     <div className="App">
-      Hello World
+      <center><h1 style={{ marginTop: "10vh", color: "white" }}>Youtube Matchmaking</h1></center>
+      <center><input type="text" className="mx-auto" placeholder="   &#xF002;   Search" style={{ width: '50vw', height: '5vh', fontFamily: 'Arial, FontAwesome', outline: 'none', padding: '10px', marginTop: "10vh" }} /></center>
+      <div style={{ width: "10vw", marginLeft: "25vw", marginTop: "3vh", float: "left" }}>
+        <Select
+          options={options}
+        />
+      </div>
+      <div style={{ width: "10vw", marginLeft: "5vw", marginTop: "3vh", float: "left" }}>
+        <Select
+          options={options}
+        />
+      </div>
     </div>
   );
 }
