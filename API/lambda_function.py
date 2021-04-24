aa39cef05572c947d40778c924dd1b2080392d7f
nivek555 <nkwyywkn@gmail.com>
2021-04-24 00:14:47 -0500

    implemented search by keywords

--- API/lambda_function.py
+++ API/lambda_function.py
@@ -1,41 +1,41 @@
 import pymongo as mongo
 from bson.json_util import dumps
-import numpy
+import numpy as np
 import json
 import shlex
-import gensim
 
 def lambda_handler(event, context):
     client = mongo.MongoClient("mongodb+srv://Nivek:Youtube123@matchmaking.3hoeh.mongodb.net/matchmaking")
     db = client.matchmaking
     query={}
     if event.get("keywords",False):
-        word_vectors = gensim.models.KeyedVectors.load('word_vectors.kv')
+        with open("word_vectors.json") as json_file:
+            word_vectors=json.load(json_file)
         keywords=shlex.split(event["keywords"].lower())
+        print("keywords", keywords)
         vector=np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
         for keyword in keywords:
             try:
-                vector+=word_vectors.get_vector(keyword)
+                vector+=np.array(word_vectors[keyword])
             except:
                 pass
         vector=vector/len(keywords)
+        print("vector", vector)
         channels=db.channels.find({})
         
         vec_dist_dict={}
         for channel in channels:
             channel_vec=np.array(channel["vector"])
             dist=np.linalg.norm(vector-channel_vec)
-            vec_dist_dict["channel"]=dist
+            vec_dist_dict[channel["title"]]=dist
         response = sorted(vec_dist_dict.items(), key=lambda x: x[1])   #List of sorted key value pairs
         response = response[(event["page"]-1)*20:event["page"]*20]
         response = [response[i][0] for i in range(len(response))]
-        response = db.channels.find({"title":{"$in":response}})
+        response = list(db.channels.find({"title":{"$in":response}}))
         return{
             "statusCode": 200,
             "body": dumps(response)
         }
-
-
 
     else:
         query["title"] = event["title"]
@@ -44,3 +44,9 @@ def lambda_handler(event, context):
             "statusCode": 200,
             "body": dumps(response)
         }
+
+lambda_handler({
+  "title": "Minecraft Parodileri",
+  "keywords":"Minecraft Nintendo",
+  "page":1
+},{})
