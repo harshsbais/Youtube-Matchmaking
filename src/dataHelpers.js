cc94557434892ecd1cc73d23594febe79b9ed4a1
Harsh Singh Bais <harshsbais5@gmail.com>
2021-05-27 19:38:47 +0530

    handle API error on frontend

--- src/dataHelpers.js
+++ src/dataHelpers.js
@@ -2,9 +2,14 @@ import axios from 'axios';
 import { _getTitleInfo } from './Endpoint';
 export const getData = async (title, lowerCountSubs, higherCountSubs, timezone, timezoneRange, val) => {
     let response;
-    if (val)
+    if (val) {
+        title = title.charAt(0).toUpperCase() + title.slice(1);
         response = await axios.get(_getTitleInfo, { params: { title, page: 1, subscriber_range: `${lowerCountSubs}-${higherCountSubs}`, timezone, timezone_range: timezoneRange } });
-    else
+
+    }
+    else {
+        title = title.toLowerCase();
         response = await axios.get(_getTitleInfo, { params: { title: false, keywords: title, page: 1, subscriber_range: `${lowerCountSubs}-${higherCountSubs}`, timezone, timezone_range: timezoneRange } });
+    }
     return response;
 }
